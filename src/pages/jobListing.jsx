35878541db7ac91480d10a93ce97f4509ccdc4ab
sammy <142742278+sammy-g-o@users.noreply.github.com>
2026-05-16 00:05:16 +0100

    Add job listing page content section with heading and description

--- src/pages/jobListing.jsx
+++ src/pages/jobListing.jsx
@@ -2,7 +2,13 @@
 
 function JobListing() {
   return (
-    <main>jobListing</main>
+    <main>
+      <section className="w-full flex flex-col gap-2">
+        <h1 className="font-extrabold font-headline text-4xl text-primary">Curated Openings</h1>
+        <p className="text-sm text-text-regular">Exclusive opportunities meticulously
+selected for the executive professional.</p>
+      </section>
+    </main>
   )
 }
 
